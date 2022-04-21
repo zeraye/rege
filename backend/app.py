f4e0e8de2645abc84ab7437744061556a0182114
zeraye <zeraye@protonmail.com>
2022-04-21 22:54:16 +0200

    feat(backend): handle voice frequency scripts

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,31 @@
 import json
-from flask import Flask, request
+import uuid
+import os
+from flask import Flask, request, abort
 from flask_cors import CORS
+from scripts.voice_frequency import voice_frequency
 
 app = Flask(__name__)
 
 CORS(app)
 
-@app.route("/rege", methods = ["POST"])
-def home():
-    print(request.data)
 
-    with open("text.wav", "wb") as f:
+@app.route("/rege", methods=["POST"])
+def rege():
+    uid = uuid.uuid4().hex
+
+    with open(f"temp/{uid}.wav", "wb") as f:
         f.write(request.data)
 
-    return json.dumps({"freq": "213.7 Hz"}), 200, {"ContentType": "application/json"}
+    try:
+        freq0 = voice_frequency(f"temp/{uid}.wav")
+    except:
+        os.remove(f"temp/{uid}.wav")
+        abort(400)
+
+    os.remove(f"temp/{uid}.wav")
+
+    return json.dumps({"freq0": str(round(freq0, 2)) + " Hz", "freq1": "Algorithm not implemented yet!"}), 200, {"ContentType": "application/json"}
 
 
 if __name__ == "__main__":
