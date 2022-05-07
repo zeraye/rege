96223aed08ff5e03aa57b3884f99046b3419721f
Patonymous <100200489+Patonymous@users.noreply.github.com>
2022-05-07 13:20:37 +0200

    Expanded search scene in App.tsx

--- frontned/src/App.tsx
+++ frontned/src/App.tsx
@@ -7,9 +7,11 @@ import Typography from "@mui/material/Typography";
 import RecordAndUpload from "./components/RecordAndUpload/RecordAndUpload";
 import Analyse from "./components/Analyse/Analyse";
 import Search from "./components/Search/Search";
+import Table, { SearchResult } from "./components/Search/Table"
+import ItemDisplay from "./components/Search/ItemDisplay"
 
 const App = () => {
-  const [scene, setScene] = useState("recordAndUpload");
+  const [scene, setScene] = useState("search");
   const [frequencies, setFrequencies] = useState<string[]>();
   const [figureUrl, setFigureUrl] = useState("");
   const [file, setFile] = useState<File | null>(null);
@@ -17,7 +19,8 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [canNext, setCanNext] = useState(false);
   const [mediaBlobUrl, setMediaBlobUrl] = useState("");
-
+  const [rows, setRows] = useState<SearchResult[]>([]);
+  
   useEffect(() => {
     setIsLoading(true);
     setIsError(false);
@@ -25,15 +28,15 @@ const App = () => {
 
   const mediaBlobUrlToFile = async (newMediaBlobUrl: string) => {
     setMediaBlobUrl(newMediaBlobUrl);
-
+    
     return await fetch(newMediaBlobUrl)
-      .then((r) => r.blob())
+    .then((r) => r.blob())
       .then(
         (blobFile) =>
           new File([blobFile], "recording.wav", { type: "audio/wav" })
-      );
+          );
   };
-
+  
   const fetchFrequencies = async () => {
     try {
       const response = await fetch("http://127.0.0.1:5000/rege", {
@@ -41,13 +44,13 @@ const App = () => {
         body: file,
         mode: "cors",
       });
-
+      
       if (!response.ok) {
         throw new Error("Error when attempting to fetch resource.");
       }
 
       const data = await response.json();
-
+      
       setIsLoading(false);
       setFrequencies([data["frequencyRudnik"], data["frequencyPochmara"]]);
       setFigureUrl(data["figureURL"]);
@@ -56,14 +59,55 @@ const App = () => {
       console.log(e);
     }
   };
-
+  
   const canNextHandler = (data: boolean = true) => {
     setCanNext(data);
   };
 
+  class Filters {
+    source: string;
+    gender: string;
+    frequency: number[];
+    constructor (source: string, gender: string, frequency: number[]) {
+      this.source = source;
+      this.gender = gender;
+      this.frequency = frequency;
+    }
+  }
+
+  const fetchSearchResults = async (filters : Filters) => {
+    try {
+      const response = await fetch("http://127.0.0.1:5000/regeSearch", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(filters),
+        mode: "cors",
+      });
+      
+      if (!response.ok) {
+        throw new Error("Error when attempting to fetch resource.");
+      }
+
+      const data = await response.json();
+      
+      setRows(data);
+    } catch (e) {
+      setIsError(true);
+      console.log(e);
+    }
+  }
+  
+  const onRowClick = (id: number) => {
+    setRows(rows.map((row) => row.indeksik === id ? {...row, higlight: true} : {...row, higlight: false}));
+    console.log(rows);
+
+    // and display stuff on the right
+  }
+
   const sceneHandler = (sceneFrom: string, sceneTo: string) => {
     if (sceneTo === "recordAndUpload") setCanNext(false);
     else if (sceneTo === "analyse") fetchFrequencies();
+    else if (sceneTo === "search") fetchSearchResults(new Filters('','',[0,0]))
     setScene(sceneTo);
   };
 
@@ -110,7 +154,26 @@ const App = () => {
         isError={isError}
       />
     );
-  else if (scene === "search") content = <Search />;
+  else if (scene === "search")
+    content = (
+      <Stack
+        direction="column"
+        spacing={2}
+        alignItems="center"
+      >
+        <Search onFiltersChanged={async (source: string,gender: string,frequency: number[]) => await fetchSearchResults(new Filters(source,gender,frequency))}/>
+        <Stack
+          direction="row"
+          spacing={2}
+        >
+          <Table
+            data={rows}
+            onRowClick={onRowClick}
+          />
+          <ItemDisplay />
+        </Stack>
+      </Stack>
+    );
 
   return (
     <Stack
