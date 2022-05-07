0cf16ffa70bcff4c59e4d5efa5858e91233e30c4
Patonymous <100200489+Patonymous@users.noreply.github.com>
2022-05-08 00:29:09 +0200

    Updated Search components
    ItemDisplay is still just a placeholder. Search bar has changed state values. Table looks better and now has paging.

--- frontned/src/components/Search/Table.tsx
+++ frontned/src/components/Search/Table.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import Stack from "@mui/material/Stack";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,80 +6,67 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import TablePagination from "@mui/material/TablePagination"
 
 export class SearchResult {
   static ID_counter = 0;
-  filename: string;
   source: string;
   gender: string;
   highlight: boolean
   frequency: number;
-  indeksik: number;
-  constructor (filename: string, source: string, gender: string, frequency: number) {
-    this.filename = filename;
+  id: number;
+  constructor (source: string, gender: string, frequency: number) {
     this.source = source;
     this.gender = gender;
     this.highlight = false;
     this.frequency = frequency;
-    this.indeksik = -20;
+    this.id = SearchResult.ID_counter;
     SearchResult.ID_counter++;
   }
 }
 
-export default function BasicTable( {data, onRowClick}: any) {
-  // const [rows, setRows] = React.useState([
-  //   {
-  //     filename: 'test1',
-  //     source: 'ja',
-  //     gender: 'apache',
-  //     higlight: false,
-  //     frequency: 0,
-  //     id: -10,
-  //   },
-  //   {
-  //     filename: 'test2',
-  //     source: 'ono',
-  //     gender: 'apache',
-  //     higlight: false,
-  //     frequency: 69,
-  //     id: -2137,
-  //   }
-  // ]);
-
-  // const onRowClick = (id: number) => {
-  //   setRows(rows.map((row) => row.id === id ? {...row, higlight: true} : {...row, higlight: false}));
-  // }
-
+export default function ResultsTable( {data, onRowClick, range, onRangeChanged, resultCount}: any) {
   return (
-    <TableContainer component={Paper} sx={{ width: '100%'}}>
-      <Table sx={{ minWidth: '50%' }} aria-label="simple table">
+    <TableContainer component={Paper} sx={{ width: 'fit-content'}}>
+      <Table sx={{ minWidth: '50%' }} aria-label='simple table'>
         <TableHead>
           <TableRow>
-            <TableCell align="left">Filename</TableCell>
             <TableCell align="center">Source</TableCell>
             <TableCell align="center">Gender</TableCell>
-            {/* <TableCell align="right">Tone</TableCell>  */}
-            <TableCell align="right">Frequency</TableCell>
+            <TableCell align="center">Frequency</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((row: SearchResult) => (
             <TableRow
-              onClick={ () => {onRowClick(row.indeksik)} }
-              key={row.indeksik}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              style={{ backgroundColor: row.highlight ? '#171717' : '#232323' }}
+              onClick={ () => {onRowClick(row.id)} }
+              key={row.id}
+              style={{ cursor: 'pointer', backgroundColor: row.highlight ? '#171717' : '#232323' }}
             >
-              <TableCell component="th" scope="row">{row.filename}</TableCell>
-              {/* <TableCell align="right">{row.filename}</TableCell> */}
               <TableCell align="center">{row.source}</TableCell>
               <TableCell align="center">{row.gender}</TableCell>
-              {/* <TableCell align="right">{row.tone}</TableCell> */}
-              <TableCell align="right">{row.frequency}</TableCell>
+              <TableCell align="center">{row.frequency}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <Stack
+        style={{ width: "100%" }}
+        spacing={2}
+        direction="row"
+        alignItems="center"
+      >
+        <TablePagination
+          align="center"
+          style={{width: '100%'}}
+          component='tr'
+          rowsPerPage={5}
+          rowsPerPageOptions={[5]}
+          count={resultCount}
+          page={range}
+          onPageChange={ (_ev, page) => {onRangeChanged(page)} }
+        />
+      </Stack>
     </TableContainer>
   );
 }
