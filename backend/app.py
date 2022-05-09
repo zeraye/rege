82b9d26ec46c7a2bc1005a3e78a232fcce477baf
zeraye <zeraye@protonmail.com>
2022-05-10 01:59:54 +0200

    feat(backend): add `search`, refactor, change endpoints

--- backend/app.py
+++ backend/app.py
@@ -2,32 +2,20 @@ import os
 import json
 import uuid
 import threading
-
-from flask import Flask, request, abort
+from dotenv import load_dotenv
+from flask import Flask, request
 from flask_cors import CORS
-
 from cloudinary import config
 from cloudinary.uploader import upload
 from cloudinary.api import delete_resources_by_tag
-
-from scripts.spectrogram import show_spectrogram
-
-from scripts.quality import check_quality
-from scripts.voice_frequency import voice_frequency
-from scripts.p_extract import voice_frequency as voice_frequency2
-
-import pydub
-import librosa
-import soundfile
-
-from dotenv import load_dotenv
+import utils
 
 load_dotenv()
 
 config(
     cloud_name=os.getenv("CLOUD_NAME"),
     api_key=os.getenv("API_KEY"),
-    api_secret=os.getenv("API_SECRET")
+    api_secret=os.getenv("API_SECRET"),
 )
 
 app = Flask(__name__)
@@ -35,68 +23,49 @@ app = Flask(__name__)
 CORS(app)
 
 
-def convert_ogg_to_wav(audio_path):
-    song = pydub.AudioSegment.from_file(audio_path)
-    song.export(audio_path, format="wav")
-
-
-@app.route("/rege", methods=["POST"])
-def rege():
+@app.route("/rege/analyse", methods=["POST"])
+def analyse():
     uid = uuid.uuid4().hex
-    audiopath = f"temp/{uid}.wav"
-    figurepath = f"figures/{uid}.png"
 
-    with open(audiopath, "wb") as f:
-        f.write(request.data)
+    audio_path = f"temp/{uid}.wav"
 
-    convert_ogg_to_wav(audiopath)
+    with open(audio_path, "wb") as file:
+        file.write(request.data)
 
-    # trim silence
-    audio, sr = librosa.load(audiopath)
-    clip = librosa.effects.trim(audio, top_db=10)
-    soundfile.write(audiopath, clip[0], sr)
+    utils.convert_ogg_to_wav(audio_path)
 
-    try:
-        frequency = int(voice_frequency(audiopath))
-        frequency2 = int(voice_frequency2(audiopath))
-        quality = check_quality(audiopath)
-        show_spectrogram(audiopath, save=True)
-        figureURL = upload(figurepath, tags=uid)["secure_url"]
-    except Exception as e:
-        print(e)
-        abort(400)
+    utils.trim_silence(audio_path)
 
-    os.remove(audiopath)
-    os.remove(figurepath)
-    threading.Timer(3600, delete_resources_by_tag, [uid]).start()
+    frequency = utils.voice_frequency3(audio_path)
 
-    return json.dumps({"frequencyRudnik": frequency,
-                       "frequencyPochmara": frequency2,
-                       "quality": quality,
-                       "figureURL": figureURL}), 200, {"ContentType": "application/json"}
+    plot_object = utils.spectrogram(audio_path, show_plot=False, dark_theme=True)
 
-@app.route("/regeSearch", methods=["POST"])
+    plot_url = upload(plot_object, tags=uid)["secure_url"]
+
+    os.remove(audio_path)
+
+    threading.Timer(300, delete_resources_by_tag, [uid]).start()
+
+    return (
+        json.dumps(
+            {
+                "frequency": int(frequency),
+                "figureURL": plot_url,
+            }
+        ),
+        200,
+        {"ContentType": "application/json"},
+    )
+
+
+@app.route("/rege/search", methods=["POST"])
 def search():
-    return json.dumps([ {"filename": "test1",
-                        "source": "debug",
-                        "gender": "apache heli",
-                        "frequency": 69 },
-                        {"filename": "test2",
-                        "source": "debug",
-                        "gender": "apache heli",
-                        "frequency": 420 },
-                        {"filename": "test3",
-                        "source": "debug",
-                        "gender": "apache heli",
-                        "frequency": 2137 },
-                        {"filename": "test4",
-                        "source": "debug",
-                        "gender": "apache heli",
-                        "frequency": 71830 },
-                        {"filename": "test5",
-                        "source": "debug",
-                        "gender": "apache heli",
-                        "frequency": 5318008 } ])
+    params = json.loads(request.data.decode("utf8"))
+
+    recordings, count = utils.search_recordings(params)
+
+    return json.dumps({"recordings": recordings, "count": count})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
