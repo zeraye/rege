b93752a55f84e47215cbe80ba689a5afc8a098fc
zeraye <zeraye@protonmail.com>
2022-05-13 23:11:35 +0200

    feat(utils): create `gender_recogniton` script

--- backend/utils/gender_recognition.py
+++ backend/utils/gender_recognition.py
@@ -1,3 +1,11 @@
-# TODO: script for detecting gender based on the voice recording
+from . import voice_frequency3
+
+# frequency ranges of male female according to
+# https://www.axiomaudio.com/blog/audio-oddities-frequency-ranges-of-male-female-and-childrens-voices/
 def gender_recognition(audio_path: str) -> str:
+    frequency = voice_frequency3(audio_path)
+
+    if frequency > 160:
+        return "female"
+
     return "male"
