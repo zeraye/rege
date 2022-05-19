6b12497432577eedc8ce5a51a8c3bebd3586511f
zeraye <zeraye@protonmail.com>
2022-05-19 20:53:04 +0200

    feat(frontend): add constants file

---
+++ frontned/src/constants.ts
@@ -0,0 +1,23 @@
+// Frequency ranges
+export const MIN_FREQUENCY = 0;
+export const MAX_FREQUENCY = 500;
+export const DELTA_FREQUENCY = 30;
+
+// Analyse page
+export const FIGURE_HEIGHT = 280;
+export const FIGURE_WIDTH = 385;
+
+// Search page
+export const RESULTS_PER_TABLE_ROW = 7;
+export const DEFAULT_TOTAL_ROWS = 50;
+export const ROW_DISPLAY_HEIGHT = 500;
+export const ROW_DISPLAY_WIDTH = 385;
+
+// Production endpoints
+export const ANALYSE_ENDPOINT =
+  'https://zeraye.pythonanywhere.com/rege/analyse';
+export const SEARCH_ENDPOINT = 'https://zeraye.pythonanywhere.com/rege/search';
+
+// Development endpoints
+// export const ANALYSE_ENDPOINT = 'http://127.0.0.1:5000/rege/analyse';
+// export const SEARCH_ENDPOINT = 'http://127.0.0.1:5000/rege/search';
